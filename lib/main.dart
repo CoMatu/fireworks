b18f93b14bd6b3ce01008dd75bad7b50f1037b61
CoMatu <30466272+CoMatu@users.noreply.github.com>
2022-02-08 20:05:31 +0300

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'dart:math';
+import 'dart:developer';
+import 'dart:math' as m;
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -18,8 +19,39 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Fireworks(
-        size: Size(400, 400),
+      home: const FireworksPage(),
+    );
+  }
+}
+
+class FireworksPage extends StatelessWidget {
+  const FireworksPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: boxDecoration,
+      child: Stack(
+        children: const [
+          Positioned(
+            top: 200,
+            right: 100,
+            child: Fireworks(
+              size: Size(300, 300),
+              itemCount: 15,
+            ),
+          ),
+          Positioned(
+            top: -100,
+            right: -50,
+            child: Fireworks(
+              size: Size(500, 500),
+              itemCount: 15,
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -39,23 +71,58 @@ class Fireworks extends StatefulWidget {
   State<Fireworks> createState() => _FireworksState();
 }
 
-class _FireworksState extends State<Fireworks> {
+class _FireworksState extends State<Fireworks> with TickerProviderStateMixin {
   final List<double> positions = [];
 
   double radius = 0.0;
+  double itemWidth = 2.0;
+  double itemWidthMax = 12.0;
+
+  late final AnimationController controller;
+  late final Animation<double> animation;
+  late final Animation<double> animationSize;
 
   @override
   void initState() {
-    // TODO: implement initState
+    controller = AnimationController(
+        vsync: this,
+        duration: const Duration(
+          milliseconds: 1000,
+        ))
+      ..addListener(() {
+        if (controller.isCompleted) {
+          controller.repeat();
+        }
+
+        // log(radius.toString());
+
+        setState(() {
+          radius = animation.value;
+          itemWidth = animationSize.value;
+        });
+      });
+    animation =
+        Tween(begin: 0.0, end: widget.size.height / 2).animate(controller);
+    animationSize = Tween(begin: 0.0, end: itemWidthMax).animate(controller);
+
+    controller.forward();
+
     super.initState();
   }
 
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SinCanvas(
-        radius: radius,
-      ),
+    return SinCanvas(
+      radius: radius,
+      itemWidth: itemWidth,
+      itemNumber: widget.itemCount,
+      size: widget.size,
     );
   }
 }
@@ -64,21 +131,26 @@ class SinCanvas extends StatelessWidget {
   const SinCanvas({
     Key? key,
     required this.radius,
+    required this.itemWidth,
+    required this.itemNumber,
+    required this.size,
   }) : super(key: key);
 
   final double radius;
+  final double itemWidth;
+  final int itemNumber;
+  final Size size;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.orangeAccent,
-        child: CustomPaint(
-          size: const Size(400, 400),
-          painter: SinPainter(
-            radius: radius,
-          ),
-        ),
+    // log(radius.toString());
+
+    return CustomPaint(
+      size: size,
+      painter: SinPainter(
+        radius: radius,
+        itemNumber: itemNumber,
+        itemWidth: itemWidth,
       ),
     );
   }
@@ -87,26 +159,28 @@ class SinCanvas extends StatelessWidget {
 class SinPainter extends CustomPainter {
   final double radius;
   final int itemNumber;
+  final double itemWidth;
 
   SinPainter({
     required this.radius,
+    required this.itemWidth,
     this.itemNumber = 12,
   });
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint()
-      ..strokeWidth = 5
+      ..strokeWidth = itemWidth
       ..strokeCap = StrokeCap.round
-      ..color = Colors.black;
+      ..color = Colors.white;
 
     final points = List.generate(
       itemNumber,
       (index) {
         final double angle =
-            (2 * pi / itemNumber) + (2 * pi / itemNumber) * (index + 1);
+            (2 * m.pi / itemNumber) + (2 * m.pi / itemNumber) * (index + 1);
         return Offset(
-          200 + radius * cos(angle),
-          200 + radius * sin(angle),
+          200 + radius * m.cos(angle),
+          200 + radius * m.sin(angle),
         );
       },
     );
@@ -117,3 +191,17 @@ class SinPainter extends CustomPainter {
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
+
+const boxDecoration = BoxDecoration(
+  gradient: LinearGradient(
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+    colors: [
+      Color(0xFFF4CD83),
+      Color(0xFFFFBB36),
+      Color(0xFFFF9A43),
+      Color(0xFFFF7257),
+      Color(0xFFFF5E4A)
+    ],
+  ),
+);
